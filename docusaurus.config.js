aaac1a51d634a0b62d0dae076eb2f5558f7758ec
DavidYu <a26364177@gmail.com>
2021-11-25 15:25:19 +0800

    add github action

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Developer note',
+  tagline: 'Developers are cool',
   url: 'https://tw-developer-note.github.io/note/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
